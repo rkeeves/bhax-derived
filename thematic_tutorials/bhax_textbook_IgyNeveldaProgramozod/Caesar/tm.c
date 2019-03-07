c389fa2abf655455684e903ef861ea3b269ee103
Norbert Bátfai <batfai.norbert@inf.unideb.hu>
2019-03-07 18:32:48 +0100

    Caesar folytat

--- thematic_tutorials/bhax_textbook_IgyNeveldaProgramozod/Caesar/tm.c
+++ thematic_tutorials/bhax_textbook_IgyNeveldaProgramozod/Caesar/tm.c
@@ -6,12 +6,16 @@ main ()
 {
     int nr = 5;
     double **tm;
-
+    
+    printf("%p\n", &tm);
+    
     if ((tm = (double **) malloc (nr * sizeof (double *))) == NULL)
     {
         return -1;
     }
 
+    printf("%p\n", tm);
+    
     for (int i = 0; i < nr; ++i)
     {
         if ((tm[i] = (double *) malloc ((i + 1) * sizeof (double))) == NULL)
@@ -21,6 +25,8 @@ main ()
 
     }
 
+    printf("%p\n", tm[0]);    
+    
     for (int i = 0; i < nr; ++i)
         for (int j = 0; j < i + 1; ++j)
             tm[i][j] = i * (i + 1) / 2 + j;
